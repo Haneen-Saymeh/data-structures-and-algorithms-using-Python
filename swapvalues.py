8f4e558676af6c69726507b960773f6013352e6e
Haninz <enghaneen_s@hotmail.com>
2023-01-18 00:25:54 +0200

    swaping values

--- swapvalues.py
+++ swapvalues.py
@@ -1,9 +1,53 @@
+# Swap values with temp
+
+def swap_temp(a,b):
+    temp = a
+    a= b
+    b= temp
+    print("value of a after swaping:", a)
+    print("value of b after swaping:", b)
+swap_temp(5,10)
+
+# Swap values without temp
+
 def swap_it(a,b):
     a,b = b,a
-    print(a,b)
+    print("value of a after swaping:", a)
+    print("value of b after swaping:", b)
 
-swap_it([1,2],[3,4])
+swap_it(5,10)
 
+# swap values with Addition and Subtraction
+
+def swap_this(a,b):
+    a= a+b
+    b= a-b
+    a= a-b
+    print("value of a after swaping:", a)
+    print("value of b after swaping:", b)
+swap_this(5,10)
+
+
+# swap values with Multiplication and Division
+
+def swap_multi(a,b):
+    a= a*b
+    b= a/b
+    a= a/b
+    print("value of a after swaping:", a)
+    print("value of b after swaping:", b)
+swap_multi(5,10)
+
+
+# XOR swap
+
+def swap_xor(a,b):
+    a= a^b
+    b= a^b
+    a= a^b
+    print("value of a after swaping:", a)
+    print("value of b after swaping:", b)
+swap_xor(5,10)
 
 
 
